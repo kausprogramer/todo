d1605575c56636744a5e8caaf918cb0bfd7db8a2
kausprogramer <kos2bh@gmail.com>
2022-06-01 18:41:45 +0530

    commit

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
 
 export default function App() {
+  const [todos, setTodos]= useState([
+    {text: 'Buy Coffee', key: 1},
+    {text: 'Complete Code Project', key: 2},
+    {text: 'Create an app', key: 3},
+    {text: 'Play on the switch', key: 4},
+  ])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/*Header*/}
+      <View styl={styles.content}>
+        {/* to Form */}
+        <FlatList
+
+        />
+      </View>
+
     </View>
   );
 }
